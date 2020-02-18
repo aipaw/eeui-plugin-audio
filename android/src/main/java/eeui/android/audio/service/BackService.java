c6b779d98a2bfbf655b135d87ab48625ed36366f
aipaw <aipaw@qq.com>
2020-02-18 12:49:32 +0800

    使用AndroidX库

--- android/src/main/java/eeui/android/audio/service/BackService.java
+++ android/src/main/java/eeui/android/audio/service/BackService.java
@@ -3,7 +3,7 @@ package eeui.android.audio.service;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 public class BackService extends Service {
 
